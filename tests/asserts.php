dca579c7ccf36ed311d3e2a39492a985f67b93dd
andy collins <anthonius.bruce@gmail.com>
2016-11-20 13:58:15 -0500

    more testing and working on an add_schema method

--- tests/asserts.php
+++ tests/asserts.php
@@ -11,4 +11,28 @@ function assert_strings_are_equal($description, $expected, $actual, $debug = fal
   }
 }
 
+function assert_bool_is_true($description, $actual, $debug = false) {
+  $test_result = " FAILED";
+  if ($actual) {
+    $test_result = " PASSED";
+  }
+
+  echo "<br \>" . $description . $test_result;
+  if ($debug) {
+    echo " actual: " . $actual;
+  }
+}
+
+function assert_bool_is_false($description, $actual, $debug = false) {
+  $test_result = " FAILED";
+  if (!$actual) {
+    $test_result = " PASSED";
+  }
+
+  echo "<br \>" . $description . $test_result;
+  if ($debug) {
+    echo " actual: " . $actual;
+  }
+}
+
 ?>
--- tests/input-format-tests.php
+++ tests/input-format-tests.php
@@ -51,8 +51,94 @@ function test_input_one_of_each_returns_all_removed() {
   assert_strings_are_equal(__FUNCTION__, $expected, $result);
 }
 
+// tests for is_valid_url
+function is_valid_url_simplewithhttp_returns_true() {
+  // Arrange
+  $in = "http://www.google.com";
+
+  // Action
+  $result = is_valid_url($in);
+
+  // Assert
+  assert_bool_is_true(__FUNCTION__, $result);
+}
+
+function is_valid_url_simplewithhttps_returns_true() {
+  // Arrange
+  $in = "https://www.google.com";
+
+  // Action
+  $result = is_valid_url($in);
+
+  // Assert
+  assert_bool_is_true(__FUNCTION__, $result);
+}
+
+function is_valid_url_simplewithwwwonly_returns_false() {
+  // Arrange
+  $in = "www.google.com";
+
+  // Action
+  $result = is_valid_url($in);
+
+  // Assert
+  assert_bool_is_false(__FUNCTION__, $result);
+}
+
+function is_valid_url_simplewithonlydomainanddotcom_returns_false() {
+  // Arrange
+  $in = "google.com";
+
+  // Action
+  $result = is_valid_url($in);
+
+  // Assert
+  assert_bool_is_false(__FUNCTION__, $result);
+}
+
+function is_valid_url_complexstring_returns_true() {
+  // Arrange
+  $in = "http://www.google.ps/search?hl=en&client=firefox-a&hs=42F&rls=org.mozilla%3Aen-US%3Aofficial&q=The+type+%27Microsoft.Practices.ObjectBuilder.Locator%27+is+defined+in+an+assembly+that+is+not+referenced.+You+must+add+a+reference+to+assembly+&aq=f&aqi=&aql=&oq=";
+
+  // Action
+  $result = is_valid_url($in);
+
+  // Assert
+  assert_bool_is_true(__FUNCTION__, $result);
+}
+
+function is_valid_url_poorlyformatedsimpleurl_returns_false() {
+  // Arrange
+  $in = "www.example.com/?p=home";
+
+  // Action
+  $result = is_valid_url($in);
+
+  // Assert
+  assert_bool_is_false(__FUNCTION__, $result);
+}
+
+function is_valid_url_poorlyformatedcomplexurl_returns_false() {
+  // Arrange
+  $in = "www.example.com/1/2/content.jsp?cid=23uolk23nlj38;o9sme;93-21-10%21%";
+
+  // Action
+  $result = is_valid_url($in);
+
+  // Assert
+  assert_bool_is_false(__FUNCTION__, $result);
+}
+
 test_input_leading_and_trailing_whitespace_returns_no_whitespace();
 test_input_backslashes_returns_no_backslashes();
 test_input_specialcharacters_returns_escaped_string();
 test_input_one_of_each_returns_all_removed();
+
+is_valid_url_simplewithhttp_returns_true();
+is_valid_url_simplewithhttps_returns_true();
+is_valid_url_simplewithwwwonly_returns_false();
+is_valid_url_simplewithonlydomainanddotcom_returns_false();
+is_valid_url_complexstring_returns_true();
+is_valid_url_poorlyformatedsimpleurl_returns_false();
+is_valid_url_poorlyformatedcomplexurl_returns_false();
 ?>
