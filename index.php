374e09836304b3085e74278233cb18060c4dd34d
andy collins <anthonius.bruce@gmail.com>
2016-11-19 22:28:51 -0500

    the URL is validated

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
 <?php
-  print <<< EOT
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Test successful</title>
-</head>
-<body>
-<h1>Test Successful</h1>
-<p>Congratulations</p>
-<p>Your webserver and PHP are working </p>
-</body>
-</html>
-EOT;
+//define variables
+$url = $urlError = "";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  if (empty($_POST["url"])) {
+    $url = "";
+  } else {
+    $url = test_input($_POST["url"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url)) {
+      $urlError = "Invalid URL"; 
+    }
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
 
+<html>
+<body>
+
+<form method="post"  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+URL: 
+<input type="text" name="url" value="<?php echo $url;?>">
+<span class="error"><?php echo $urlError;?></span>
+<br><br>
+<input type="submit" name="submit" value="Submit"> 
+</form>
+
+</body>
+</html>
